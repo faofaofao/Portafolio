9fbcd576a4cca1032be101413b13ac477fab1c6a
Pedro Cuevas <pacuevasl@gmail.com>
2024-08-03 00:35:02 -0400

    edit: cambio valor para connectmongo db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, { 
+    const conn = await mongoose.connect('mongodb+srv://pacuevasl:faofao123456@portafolio.g2esmw4.mongodb.net/?retryWrites=true&w=majority&appName=Portafolio', { 
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
