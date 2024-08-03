9c2720bc82baabb5dd76cc2c591dbdce93b26c4c
Pedro Cuevas <pacuevasl@gmail.com>
2024-08-03 18:52:32 -0400

    edit: config cors index.js

--- server/index.js
+++ server/index.js
@@ -1,24 +1,26 @@
-// server/index.js
 const express = require('express');
-const connectDB = require('./config/db');
 const cors = require('cors');
-const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const contactRoutes = require('./routes/contact');
 
-// Cargar variables de entorno
-dotenv.config();
+const app = express();
 
 // Conectar a la base de datos
 connectDB();
 
-const app = express();
-
-// Middleware
+// Middleware para parsear JSON
 app.use(express.json());
-app.use(cors());
+
+// Configurar CORS
+app.use(cors({
+  origin: 'https://portafolio-frontend-b92fhgf1c-pedros-projects-ba5e240c.vercel.app', // Reemplaza con la URL de tu frontend
+  optionsSuccessStatus: 200
+}));
 
 // Rutas
-app.use('/api/contact', require('./routes/contact'));
+app.use('/api/contact', contactRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
