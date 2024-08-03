e00ce2556933ffb4565ad9674d9f9dfda2c5a8d9
Pedro Cuevas <pacuevasl@gmail.com>
2024-08-03 00:40:48 -0400

    edit: cambio URL mongoodb

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://pacuevasl:faofao123456@portafolio.g2esmw4.mongodb.net/?retryWrites=true&w=majority&appName=Portafolio', { 
+    const conn = await mongoose.connect('mongodb+srv://pacuevasl:faofao123456@ac-zvkyhsl.mongodb.net/Portafolio?retryWrites=true&w=majority', { 
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
