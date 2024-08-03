432b13e8c43dc9560e45eb23c5c5e0511c1ce304
Pedro Cuevas <pacuevasl@gmail.com>
2024-08-03 14:51:40 -0400

    edit:config:db.js, cambio de codigo require dotenv

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,19 @@
-// server/config/db.js
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, { 
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log('MongoDB Connected');
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error('Error connecting to MongoDB:', error.message);
     process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
 
