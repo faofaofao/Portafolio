a492738f1a7b9efb26347f1dfd9b7ca9598b6319
Pedro Cuevas <pacuevasl@gmail.com>
2024-08-03 21:29:25 -0400

    edit: configuracion de rutas en el backend y frontend contact.js Contact.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cors({
 app.use(express.json());
 
 // Rutas
-app.use('./pages/api/contact', require('./routes/contact')); // Asegúrate de que la ruta sea correcta
+app.use('api/contact', require('./routes/contact')); // Asegúrate de que la ruta sea correcta
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
